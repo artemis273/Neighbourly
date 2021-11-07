2eb3d51b7e5bfc08758f3240530cb16987ff76a0
dallasdoherty <76972829+dallasdoherty@users.noreply.github.com>
2021-11-06 23:15:42 -0400

    Update donationApp.py

--- donationApp.py
+++ donationApp.py
@@ -46,37 +46,92 @@ def request():
     print ("What article of clothing are you looking for?\nThe options are as follows\nwinter coats\nfall coats\ntops\nbottoms\nsneakers\nboots\nsocks")
     category = input("Type your selection")
 
-     if category == 'winter coats':
+    if category == 'winter coats':
         size = input("What size is the coat?")
-        for size in clothing[category]:
-            print('Match Found' + size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
 
     if category == 'fall coats':
         size = input("What size is the coat")
-        clothing['fall coats'].append(size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
 
     if category == 'tops':
         size = input("What size is the top")
-        clothing['tops'].append(size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded.append(size)
 
     if category == 'bottoms':
         size = input('What size are the bottoms')
-        clothing['bottoms'].append(size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
 
     if category == 'sneakers':
         size = input("What size are the sneakers?")
-        clothing['sneakers'].append(size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
 
     if category == 'boots':
         size = input("What size are the boots?")
-        clothing['boots'].append(size)
-
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
+            
     if category == 'socks':
         size = input("What size are the boots?")
-        clothing['socks'].append(size)
+        if size in clothing[category]:
+            print('Match Found ' + size)
+            pendingPickup[category].append(size)
+            clothing[category].remove(size)
+        else:
+            itemsNeeded[category].append(size)
 
     else:
-        print('Article of clothing not found')
+        print("Category of clothing not found")
+
+
+def checkMatches():
+    '''
+    iterates through both dictionaries and removes the matches, sends them to pendingPickup dict
+    '''
+                
     
     
-give()
+
+def main():
+    x = input("Type Donate to add a donation or Type Request to request an item")
+
+    if x == Donate:
+        give()
+    if x == Request:
+        request()
+    else:
+        print("Invalid input")
+
+    #checkMatches()
+            
+            
+main()
